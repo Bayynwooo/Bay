238a3e8e2a4f23510225a87943dfcee80ce7a92b
Bayynwooo <np007913@gmail.com>
2026-04-24 11:53:56 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 const databaseProyek = {
     "1": {
-        judul: "Portofolio Pribadi",
+        judul: "PORTOFOLIO PRIBADI",
         keahlian: ['Desainer,Ahli PHP,HTML,dan JavaSript'],
-        gambar: "https://course-net.com/wp-content/uploads/2025/03/h2917prj7ldo0ow5x5ih.webp",
-        latar: "Ini adalah projek milik saya yang di kembangkan dengan syntax html,php,dan javaScript",
+        gambar: "https://lh7-us.googleusercontent.com/WqrPPrutd_FF1k1Y68R58IZlnP6Cw4IggfMCUxRKs15oYJuJagX3jnU7rYuW2p_TUWEccfQGGQaRSEezKTKfsz-Zd-1eQmxmU69icsJDRWiAXMsdeiIf21Z49S8JeUhr1Gqqm_TOYQlJIP7GaRNN5I8",
+        latar: "Ini adalah projek milik saya yang di kembangkan dengan syntax html,php,dan javaScript.",
         tantangan: "Meningkatkan kemampuan Anda meliputi pemahaman logika dasar, debugging error, dan adaptasi dengan teknologi baru.",
         pelajaran: "Pemahaman logika (algoritma), pemilihan bahasa (seperti Python untuk pemula), dan praktik langsung melalui proyek kecil.",
         link: ""
