39e3e3a0e7594d777dd7c04f6d83648f0814699f
Bayynwooo <np007913@gmail.com>
2026-04-24 09:23:20 +0700

    upload github

---
+++ data.js
@@ -0,0 +1,11 @@
+const databaseProyek = {
+    "1": {
+        judul: "Portofolio Pribadi",
+        keahlian: ['Desainer,Ahli PHP,HTML,dan JavaSript'],
+        gambar: "https://course-net.com/wp-content/uploads/2025/03/h2917prj7ldo0ow5x5ih.webp",
+        latar: "Ini adalah projek milik saya yang di kembangkan dengan syntax html,php,dan javaScript",
+        tantangan: "Meningkatkan kemampuan Anda meliputi pemahaman logika dasar, debugging error, dan adaptasi dengan teknologi baru.",
+        pelajaran: "Pemahaman logika (algoritma), pemilihan bahasa (seperti Python untuk pemula), dan praktik langsung melalui proyek kecil.",
+        link: ""
+    },
+};
